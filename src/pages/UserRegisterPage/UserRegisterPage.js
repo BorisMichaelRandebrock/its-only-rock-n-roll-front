635e526d01da09d2693084cf5213765bd6638b43
Boris <boris@randebrock.com>
2022-06-04 17:37:49 +0200

    added UserRegisterPage and corresponding test

---
+++ src/pages/UserRegisterPage/UserRegisterPage.js
@@ -0,0 +1,13 @@
+import RegisterFormStyle from "../../components/RegisterForm/RegisterFormStyle";
+import RegisterForm from "../../components/RegisterForm/RegisterForm";
+
+const UserRegisterPage = () => {
+  return (
+    <RegisterFormStyle>
+      <h1>Register</h1>
+      <RegisterForm />
+    </RegisterFormStyle>
+  );
+};
+
+export default UserRegisterPage;
