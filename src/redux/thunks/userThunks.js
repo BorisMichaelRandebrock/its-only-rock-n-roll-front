b6a567a130869790ad5e03d33c1c2390af728e89
Boris <boris@randebrock.com>
2022-06-04 16:32:02 +0200

    added try-catch to login

--- src/redux/thunks/userThunks.js
+++ src/redux/thunks/userThunks.js
@@ -7,14 +7,18 @@ export const registerThunk = (userData) => async (dispatch) => {
 };
 
 export const loginThunk = (userData) => async (dispatch) => {
-  const { data, status } = await axios.post(
-    `${process.env.REACT_APP_API_URL}user/login`,
-    userData
-  );
+  try {
+    const { data, status } = await axios.post(
+      `${process.env.REACT_APP_API_URL}user/login`,
+      userData
+    );
 
-  if (status === 200) {
-    const { id, username } = jwtDecode(data.token);
-    localStorage.setItem("token", data.token);
-    dispatch(loginActionCreator({ id, username }));
+    if (status === 200) {
+      const { id, username } = jwtDecode(data.token);
+      localStorage.setItem("token", data.token);
+      dispatch(loginActionCreator({ id, username }));
+    }
+  } catch (error) {
+    return error;
   }
 };
