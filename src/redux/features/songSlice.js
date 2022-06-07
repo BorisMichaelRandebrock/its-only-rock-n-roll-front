3edafb1aaccaa2bfa979b5e52990a258c45f9366
Boris <boris@randebrock.com>
2022-06-07 13:10:43 +0200

    added tested songSlice

---
+++ src/redux/features/songSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const songSlice = createSlice({
+  name: "song",
+  initialState: [],
+  reducers: {
+    loadSongs: (song, action) => ({ ...action.payload }),
+  },
+});
+
+export const { loadSongs: loadSongsActionCreator } = songSlice.actions;
+
+export default songSlice.reducer;
